a4f0b3cd6211a30a1b1d3b8e70335dfd57b27ffc
RobinKhan <arahaman235@gmail.com>
2021-12-31 12:16:35 +0600

    first

---
+++ src/main/java/com/redhat/redhatopenshift/RedhatopenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.redhat.redhatopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedhatopenshiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedhatopenshiftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redhat/redhatopenshift/RedhatopenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhat.redhatopenshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedhatopenshiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
